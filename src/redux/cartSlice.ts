ae7dce83f967566db7cd7efe1f86dd9654f6a2de
remy_oluwatomi <remioluwatomicephas@gmail.com>
2025-04-12 08:15:25 -0400

    fix: corrected unused type build error

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,4 +1,4 @@
-// import type * as rtk from "@reduxjs/toolkit";
+import type * as _rtk from "@reduxjs/toolkit";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "../supabase/supabase";
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import type * as rtk from "@reduxjs/toolkit";
+import type * as _rtk from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./cartSlice";
 
